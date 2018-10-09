42e4e3c35a944a1f5f1d86bebec9ec794fd4a99c
Lucas Kramer <krame505@umn.edu>
2018-10-09 17:59:02 -0500

    Adding arduino software

---
+++ FlightTelemetry/FlightTelemetry.ino
@@ -0,0 +1,112 @@
+#include <Wire.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <SD.h>
+#include <RTClib.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <avr/pgmspace.h>
+#include <Telemetry.h>
+
+long    launchTime = 0;
+int     accel;
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Servo servo1;
+Servo servo2;
+RTC_DS1307 RTC;
+File dataFile;
+char filename[] = "DATA000.csv";
+
+#define LED           10
+#define data_size     3         // uses 0 (data_size-1 valid data points)
+#define loopDelay     100       // min 200 to avoid missing deadlines (100 is fairly stable)
+#define SDdelay       10
+#define flagIncrement 10
+#define sdErrorLimit  2
+#define saveInterval  3000
+#define dataTime ((float)loopDelay)/1000      // time between data
+float eulerNew,eulerOld;
+int i, j;
+int flag = 0;
+long checkSD,saveFlag;
+
+#define SEND_VECTOR_ITEM(field, value)\
+  SEND_ITEM(field, value.x())         \
+  SEND_GROUP_ITEM(value.y())          \
+  SEND_GROUP_ITEM(value.z())
+
+#define WRITE_CSV_ITEM(value)         \
+  dataFile.print(F(", ")); dataFile.print(value);
+
+#define WRITE_CSV_VECTOR_ITEM(value)  \
+  WRITE_CSV_ITEM(value.x())           \
+  WRITE_CSV_ITEM(value.y())           \
+  WRITE_CSV_ITEM(value.z())
+
+unsigned int missed_deadlines = 0;
+
+
+void setup() {
+  Serial.begin(38400, SERIAL_8N2);    Serial.println();
+  
+  while (!bno.begin()) {                            // flashes to signal error
+    Serial.println(F("BNO055 err"));
+    digitalWrite(LED,LOW); delay(1000); digitalWrite(LED,HIGH);
+  }
+  if (!RTC.isrunning()) { RTC.adjust(DateTime(__DATE__, __TIME__)); }
+  if (!SD.begin(10)) { Serial.println(F("SD err")); }
+  else {                                            // generates file name
+    for (uint16_t nameCount = 0; nameCount < 1000; nameCount++) {
+      filename[4] = nameCount/100 + '0';
+      filename[5] = (nameCount%100)/10 + '0';
+      filename[6] = nameCount%10 + '0';
+      if (!SD.exists(filename)) {                   // opens if file doesn't exist
+        dataFile = SD.open(filename, FILE_WRITE);
+        Serial.print(F("\twriting "));
+        Serial.println(filename);
+        dataFile.println(F("abs time,sys date,sys time,x_euler_angle,y_euler_angle,z_euler_angle,x_gyro,y_gyro,z_gyro,temperature,x_magnetometer,y_magnetometer,z_magnetometer,x_accelerometer,y_accelerometer,z_accelerometer,"));
+        break;
+      }
+    }
+  }
+  
+}
+
+
+void loop() {
+  long time0 = millis();
+  imu::Vector<3> gyroscope     = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  imu::Vector<3> euler         = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  imu::Vector<3> accelerometer = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu::Vector<3> magnetometer  = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  int8_t temp = bno.getTemp();
+  
+  // Downlink
+  BEGIN_SEND
+  SEND_VECTOR_ITEM(euler_angle ,  euler);
+  SEND_VECTOR_ITEM(gyro        ,  gyroscope);
+  SEND_ITEM(temperature, temp);
+  SEND_VECTOR_ITEM(magnetometer,  magnetometer);
+  SEND_VECTOR_ITEM(acceleration,  accelerometer);
+  END_SEND
+  
+  // Writing to SD Card
+  DateTime now = RTC.now();           checkSD = millis();     // checks for SD removal
+  dataFile.print(millis());           dataFile.print(',');
+  dataFile.print(now.year()  ,DEC);   dataFile.print('/');
+  dataFile.print(now.month() ,DEC);   dataFile.print('/');
+  dataFile.print(now.day()   ,DEC);   dataFile.print(',');
+  dataFile.print(now.hour()  ,DEC);   dataFile.print(':');
+  dataFile.print(now.minute(),DEC);   dataFile.print(':');
+  dataFile.print(now.second(),DEC);
+  WRITE_CSV_VECTOR_ITEM(euler)
+  WRITE_CSV_VECTOR_ITEM(gyroscope)
+  WRITE_CSV_ITEM(temp)
+  WRITE_CSV_VECTOR_ITEM(magnetometer)
+  WRITE_CSV_VECTOR_ITEM(accelerometer)
+  
+  dataFile.println();
+  dataFile.flush();
+}
